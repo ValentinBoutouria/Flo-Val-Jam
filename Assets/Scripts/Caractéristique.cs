50b0a705d50ad9bb0f26825f6bca1f3d6657ba4e
ValentinBoutouria <151946292+ValentinBoutouria@users.noreply.github.com>
2024-05-03 20:38:23 +0100

    deplacement running

--- Assets/Scripts/Caractéristique.cs
+++ Assets/Scripts/Caractéristique.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Caractéristique : MonoBehaviour
 
 {
+    public float walkingSpeed = 5f;
     public float speed = 5f;
+    public float runningSpeed = 10f;
     public float jumpForce = 2f;
+    public float life = 100f;
     public bool isGrounded;
+    public bool isWalking;
+    public bool isRunning;
+    public bool isDashing;
 
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,19 +27,42 @@ public class Caractéristique : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        DeplacementPersoSimple();
+        DeplacementWalk();
+        DeplacementRun();
         Jump();
 
 
+
     }
-    void DeplacementPersoSimple() {
+    void DeplacementWalk() {
         // Déplacement horizontal
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(verticalInput, 0f, 0f) * speed * Time.deltaTime;
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * speed * Time.deltaTime;
         transform.Translate(movement);
 
         
+    }
+    void DeplacementRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isRunning= true;
+            isWalking = false;
+            speed = runningSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isRunning= false;
+            isWalking= true;
+            speed = walkingSpeed;
+
+        }
+
+    }
+    void ControleWalk()
+    {
+
     }
 
     void Jump()
@@ -41,7 +70,8 @@ public class Caractéristique : MonoBehaviour
         // Gestion du saut
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);  
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+  
         }
 
     }
