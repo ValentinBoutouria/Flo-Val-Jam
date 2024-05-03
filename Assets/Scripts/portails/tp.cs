76e6b50353aa2f7f1098eb8ea6a00dfd11996311
Florian BOISSET <florian.boisset31@gmail.com>
2024-05-03 17:21:16 -0300

    tp ok

--- Assets/Scripts/portails/tp.cs
+++ Assets/Scripts/portails/tp.cs
@@ -8,7 +8,11 @@ public class tp : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        // Lorsqu'un objet entre dans le collider, changez sa position à celle de l'autre prefab
-        other.transform.position = otherPrefab.transform.position;
+        // Calculer l'écart entre le collider other et le collider de l'objet
+        Vector3 offset = other.transform.position - this.transform.position;
+        offset.y = 0; // Ignorer la hauteur
+        // Ajouter cet écart à la position de l'autre prefab
+        other.transform.position = otherPrefab.transform.position + offset*-2;
+
     }
 }
