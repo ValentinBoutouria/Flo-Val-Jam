674afca252b04b35ade61dde9c269dd10d95f68b
Florian BOISSET <florian.boisset31@gmail.com>
2024-05-03 20:15:06 -0300

    equipement desequipement

--- Assets/Scripts/raycastEquipement.cs
+++ Assets/Scripts/raycastEquipement.cs
@@ -10,21 +10,64 @@ public class raycastEquipement : MonoBehaviour
     public TextMeshProUGUI textMeshPro; // Référence au TextMeshPro
     public Equipement equipement; // Référence au script Equipement
 
+    private string currentTag;
+    private string currentName;
+
     void Update()
     {
         // Lance un raycast à partir de la caméra
         RaycastHit hit;
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit))
         {
+            Debug.DrawRay(cam.transform.position, cam.transform.forward * hit.distance, Color.red);
+
             // Vérifie si l'objet touché a le tag "epee" ou d'autres tags spécifiques
             if (hit.collider.CompareTag("epee") /* || hit.collider.CompareTag("autreTag") */)
             {
-                // Affiche "equiper" suivi du tag et du nom de l'objet dans le TextMeshPro
-                textMeshPro.text = "Equiper " + hit.collider.tag + " " + hit.collider.name;
+                // Stocke le tag et le nom de l'objet actuellement visé
+                currentTag = hit.collider.tag;
+                currentName = hit.collider.name;
 
-                // Ajoute l'élément correspondant à EquippedStuff depuis GameStuff
-                equipement.EquipItem(hit.collider.tag, hit.collider.name);
+                // Vérifie si l'épée est déjà équipée
+                if (equipement.EquippedStuff.ContainsKey(currentTag) && equipement.EquippedStuff[currentTag].ContainsKey(currentName))
+                {
+                    // Si l'épée est déjà équipée, affiche un message pour la déséquiper
+                    textMeshPro.text = "Appuyez sur E pour déséquiper " + currentTag + " " + currentName;
+                }
+                else
+                {
+                    // Si l'épée n'est pas équipée, affiche un message pour l'équiper
+                    textMeshPro.text = "Appuyez sur E pour équiper " + currentTag + " " + currentName;
+                }
+            }
+            else
+            {
+                // Si l'objet touché n'a pas le tag "epee" ou d'autres tags spécifiques, efface le texte et réinitialise le tag et le nom actuels
+                textMeshPro.text = "";
+                currentTag = null;
+                currentName = null;
+            }
+        }
+        else
+        {
+            // Si le raycast ne touche pas d'objet, efface le texte et réinitialise le tag et le nom actuels
+            textMeshPro.text = "";
+            currentTag = null;
+            currentName = null;
+        }
+
+        // Si le joueur appuie sur E, équipe ou déséquipe l'épée
+        if (Input.GetKeyDown(KeyCode.E) && currentTag != null && currentName != null)
+        {
+            if (equipement.EquippedStuff.ContainsKey(currentTag) && equipement.EquippedStuff[currentTag].ContainsKey(currentName))
+            {
+                equipement.EquippedStuff[currentTag].Remove(currentName);
+            }
+            else
+            {
+                equipement.EquipItem(currentTag, currentName);
             }
         }
     }
+ 
 }
